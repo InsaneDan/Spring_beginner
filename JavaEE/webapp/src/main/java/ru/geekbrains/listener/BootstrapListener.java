c50b70ce21b2310f4d86b7b161fa5729fff77576
IDA <isakov.danila.79@gmail.com>
2021-04-06 02:45:02 +0200

    Lesson_01 homework
    JavaEE simple servlet demo (HttpServlet+JSP)

---
+++ JavaEE/webapp/src/main/java/ru/geekbrains/listener/BootstrapListener.java
@@ -0,0 +1,24 @@
+package ru.geekbrains.listener;
+
+import ru.geekbrains.persistence.Product;
+import ru.geekbrains.persistence.ProductRepository;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.math.BigDecimal;
+
+@WebListener
+public class BootstrapListener implements ServletContextListener {
+
+    // создание списка продуктов при инициализации сервлета
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ProductRepository productRepository = new ProductRepository();
+        for (int i = 1; i < 11; i++) {
+            productRepository.saveOrUpdate(new Product(null, "Product " + i,
+                    "Description of product " + i, new BigDecimal(i * 100)));
+        }
+        sce.getServletContext().setAttribute("productRepository", productRepository);
+    }
+}
