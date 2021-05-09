c8d0255c851d5d7f18f07449062a61fd3c19699f
IDA <isakov.danila.79@gmail.com>
2021-05-09 22:28:59 +0200

    Lesson 10. REST API - orders

---
+++ Lesson_10. Spring REST 2/src/main/java/ru/geekbrains/persistence/dtos/ProductDto.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.persistence.dtos;
+
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+public class ProductDto {
+
+    private Long id;
+    private String title;
+    private BigDecimal price;
+
+}
---
+++ Lesson_10. Spring REST 2/src/main/java/ru/geekbrains/persistence/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.persistence.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.geekbrains.persistence.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
