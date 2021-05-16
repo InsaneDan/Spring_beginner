011f8478bd029e54828f454705f21e14650c98a0
IDA <isakov.danila.79@gmail.com>
2021-05-16 17:35:31 +0200

    Lesson 11 Spring Security (in memory, jdbc, spring data/dao)

---
+++ Lesson_11. Spring Security/src/main/java/ru/geekbrains/controllers/GlobalController.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequiredArgsConstructor
+public class GlobalController {
+
+    private final UserDetailsService userDetailsService;
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home";
+    }
+
+    @GetMapping("/unsecured")
+    public String usecuredPage() {
+        return "unsecured";
+    }
+
+    @GetMapping("/auth")
+    public String authPage() {
+        return "authenticated";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    public String userPageAuthentication(Authentication authentication) {
+        return "Authenticated user info: " + authentication.getName() + " authorities: " + authentication.getAuthorities();
+    }
+
+    @GetMapping("/user_principal")
+    public String userPagePrincipal(Principal principal) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(principal.getName());
+        return "Authenticated user info: " + userDetails.getUsername() + " authorities: " + userDetails.getAuthorities();
+    }
+
+}
